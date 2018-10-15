820018fff5d66130640c7be108f3ae1b79441c94
bert hubert <bert.hubert@netherlabs.nl>
2018-10-15 18:16:51 +0200

    add a lot of comments, add actual 'hint' procedure

--- tdns/tres.cc
+++ tdns/tres.cc
@@ -16,10 +16,12 @@ using namespace std;
 
 multimap<DNSName, ComboAddress> g_root;
 unsigned int g_numqueries;
+bool g_skipIPv6{true}; 
 
 ComboAddress getIP(const std::unique_ptr<RRGen>& rr)
 {
   ComboAddress ret;
+  ret.sin4.sin_family = 0;
   if(auto ptr = dynamic_cast<AGen*>(rr.get()))
     ret=ptr->getIP();
   else if(auto ptr = dynamic_cast<AAAAGen*>(rr.get()))
@@ -47,14 +49,16 @@ DNSMessageReader getResponse(const ComboAddress& server, const DNSName& dn, cons
   bool doEDNS=true, doTCP=false;
 
   for(int tries = 0; tries < 4 ; ++tries) {
-    if(++g_numqueries > 30)
-      throw TooManyQueriesException();
+    if(++g_numqueries > 30) // there is the possibility our algorithm will loop
+      throw TooManyQueriesException(); // and send out thousands of queries, so let's not
+
     DNSMessageWriter dmw(dn, dt);
     dmw.dh.rd = false;
     dmw.randomizeID();
-    if(doEDNS)
-      dmw.setEDNS(700, true);
+    if(doEDNS) 
+      dmw.setEDNS(1500, true); 
     string resp;
+    double timeout=1.0;
     if(doTCP) {
       Socket sock(server.sin4.sin_family, SOCK_STREAM);
       SConnect(sock, server);
@@ -64,15 +68,29 @@ DNSMessageReader getResponse(const ComboAddress& server, const DNSName& dn, cons
       SWrite(sock, tmp);
       SWrite(sock, ser);
 
+      int err = waitForData(sock, &timeout);
+
+      if( err <= 0) {
+        throw std::runtime_error("Error waiting for data from "+server.toStringWithPort()+": "+ (err ? string(strerror(errno)): string("Timeout")));
+      }
+
       tmp=SRead(sock, 2);
       len = ntohs(*((uint16_t*)tmp.c_str()));
+
+      // so yes, you need to check for a timeout here again!
+      err = waitForData(sock, &timeout);
+
+      if( err <= 0) {
+        throw std::runtime_error("Error waiting for data from "+server.toStringWithPort()+": "+ (err ? string(strerror(errno)): string("Timeout")));
+      }
+      // and even this is not good enough, an authoritative server could be trickling us bytes
       resp = SRead(sock, len);
     }
     else {
       Socket sock(server.sin4.sin_family, SOCK_DGRAM);
       SConnect(sock, server);
       SWrite(sock, dmw.serialize());
-      double timeout=1.0;
+
       int err = waitForData(sock, &timeout);
 
       // so one could simply retry on a timeout, but here we don't
@@ -87,7 +105,7 @@ DNSMessageReader getResponse(const ComboAddress& server, const DNSName& dn, cons
       cout << prefix << "ID mismatch on answer" << endl;
       continue;
     }
-    if(!dmr.dh.qr) {
+    if(!dmr.dh.qr) { // for security reasons, you really need this
       cout << prefix << "What we received was not a response, ignoring"<<endl;
       continue;
     }
@@ -104,6 +122,7 @@ DNSMessageReader getResponse(const ComboAddress& server, const DNSName& dn, cons
     return dmr;
   }
   // should never get here
+  return DNSMessageReader(""); // just to make compiler happy
 }
 
 // this is a different kind of error: we KNOW your thing does not exist
@@ -122,14 +141,17 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
   prefix += dn.toString() + "|"+toString(dt)+" ";
  
   vector<std::unique_ptr<RRGen>> ret;
+
   for(auto& sp : servers) {
     ret.clear();
     ComboAddress server=sp.second;
     server.sin4.sin_port = htons(53);
-
+    
+    if(g_skipIPv6 && server.sin4.sin_family == AF_INET6)
+      continue;
     try {
       cout << prefix<<"Sending to server "<<sp.first<<" on "<<server.toString()<<endl;
-      DNSMessageReader dmr = getResponse(server, dn, dt, depth); // takes care of EDNS and TCP
+      DNSMessageReader dmr = getResponse(server, dn, dt, depth); // takes care of EDNS and TCP for us
 
       DNSSection rrsection;
       uint32_t ttl;
@@ -137,13 +159,15 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
       DNSName rrdn;
       DNSType rrdt;
       
-      dmr.getQuestion(rrdn, rrdt);
+      dmr.getQuestion(rrdn, rrdt); // parse
+      
       cout << prefix<<"Received response with RCode "<<(RCode)dmr.dh.rcode<<", qname " <<dn<<", qtype "<<dt<<", aa: "<<dmr.dh.aa << endl;
       if(rrdn != dn || dt != rrdt) {
         cout << prefix << "Got a response to a different question or different type than we asked for!"<<endl;
         continue; // see if another server wants to work with us
       }
 
+      // in a real resolver, you must ignore NXDOMAIN in case of a CNAME. Because that is how the internet rolls.
       if((RCode)dmr.dh.rcode == RCode::Nxdomain) {
         cout << prefix<<"Got an Nxdomain, it does not exist"<<endl;
         throw NxdomainException();
@@ -158,6 +182,11 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
       std::unique_ptr<RRGen> rr;
       set<DNSName> nsses;
       multimap<DNSName, ComboAddress> addresses;
+
+      /* here we loop over records. Perhaps the answer is there, perhaps
+         there is a CNAME we should follow, perhaps we get a delegation.
+         And if we do get a delegation, there might even be useful glue */
+      
       while(dmr.getRR(rrsection, rrdn, rrdt, ttl, rr)) {
         cout << prefix << rrsection<<" "<<rrdn<< " IN " << rrdt << " " << ttl << " " <<rr->toString()<<endl;
         if(dmr.dh.aa==1) {
@@ -172,6 +201,9 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
           }
         }
         else {
+          // this picks up nameserver records, and we even believe your glue.. but ONLY for this query
+          // note that this should check if the NS records are even relevant for the query
+          // from a security perspective, all an auth can do is ruin the result, since we don't cache
           if(rrsection == DNSSection::Authority && rrdt == DNSType::NS)
             nsses.insert(dynamic_cast<NSGen*>(rr.get())->d_name);
           else if(rrsection == DNSSection::Additional && nsses.count(rrdn) && (rrdt == DNSType::A || rrdt == DNSType::AAAA)) {
@@ -180,6 +212,7 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
         }
       }
       if(!ret.empty()) {
+        // the answer is in!
         cout << prefix<<"Done, returning "<<ret.size()<<" results\n";
         return ret;
       }
@@ -188,6 +221,7 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
         throw NodataException();
       }
       if(!addresses.empty()) {
+        // in addresses are nameservers for which we have IP or IPv6 addresses
         cout << prefix<<"Have "<<addresses.size()<<" IP addresses to iterate to: ";
         for(const auto& p : addresses)
           cout << p.first <<"="<<p.second.toString()<<" ";
@@ -197,7 +231,9 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
           return res2;
         cout << prefix<<"The IP addresses we had did not provide a good answer"<<endl;
       }
-      
+
+      // well we could not make it work using the servers we had addresses for. Let's try
+      // to get addresses for the rest
       cout << prefix<<"Don't have a resolved nameserver to ask anymore, trying to resolve "<<nsses.size()<<" names"<<endl;
 
       for(const auto& name: nsses) {
@@ -218,15 +254,19 @@ vector<std::unique_ptr<RRGen>> resolveAt(const DNSName& dn, const DNSType& dt, i
         cout << prefix<<"We now have "<<newns.size()<<" resolved addresses to try"<<endl;
         if(newns.empty())
           continue;
+
+        // we have a new (set) of addresses to try
         auto res2 = resolveAt(dn, dt, depth+1, newns);
-        if(!res2.empty())
+        if(!res2.empty()) // it worked!
           return res2;
+        // it didn't, let's move on to the next server
       }
     }
     catch(std::exception& e) {
       cout << prefix <<"Error resolving: " << e.what() << endl;
     }
   }
+  // if we get here, we have no results for you.
   return ret;
 }
 
@@ -237,13 +277,35 @@ try
     cerr<<"Syntax: tres name type\n";
     return(EXIT_FAILURE);
   }
+  signal(SIGPIPE, SIG_IGN); // TCP, so we need this
+  // configure some hints
+  multimap<DNSName, ComboAddress> hints = {{makeDNSName("a.root-servers.net"), ComboAddress("198.41.0.4", 53)},
+                                           {makeDNSName("f.root-servers.net"), ComboAddress("192.5.5.241", 53)},
+                                           {makeDNSName("k.root-servers.net"), ComboAddress("193.0.14.129", 53)},
+  };
 
-  g_root = {{makeDNSName("a.root-servers.net"), ComboAddress("198.41.0.4", 53)},
-            {makeDNSName("f.root-servers.net"), ComboAddress("192.5.5.241", 53)},
-            {makeDNSName("k.root-servers.net"), ComboAddress("193.0.14.129", 53)},
-  };;
-  
-  signal(SIGPIPE, SIG_IGN);
+  // retrieve the actual live NSSET from the hints
+  for(const auto& h : hints) {
+    try {
+      DNSMessageReader dmr = getResponse(h.second, makeDNSName("."), DNSType::NS);
+      DNSSection rrsection;
+      DNSName rrdn;
+      DNSType rrdt;
+      uint32_t ttl;
+      std::unique_ptr<RRGen> rr;
+      // this assumes the root will only send us relevant NS records
+      // we could check with the NS records if we wanted
+      // but if a root wants to mess with us, it can
+      while(dmr.getRR(rrsection, rrdn, rrdt, ttl, rr)) {
+        if(rrdt == DNSType::A || rrdt == DNSType::AAAA)
+          g_root.insert({rrdn, getIP(rr)});
+      }
+      break;
+    }
+    catch(...){}
+  }
+
+  cout<<"Retrieved . NSSET from hints, have "<<g_root.size()<<" addresses"<<endl;
 
   DNSName dn = makeDNSName(argv[1]);
   DNSType dt = makeDNSType(argv[2]);
